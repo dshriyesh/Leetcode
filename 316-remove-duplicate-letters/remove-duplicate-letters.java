a783f67fb939191baf9f9d12c6a05d03b3ad39b3
dshriyesh <155504862+dshriyesh@users.noreply.github.com>
2025-04-10 19:57:55 +0530

    Time: 2 ms (94.58%) | Memory: 42.4 MB (63.25%) - LeetSync

---
+++ 316-remove-duplicate-letters/remove-duplicate-letters.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String removeDuplicateLetters(String s) {
+        boolean[] bool= new boolean[26];
+        int[] position= new int[26];
+        for(int i=0;i<s.length();i++){
+            position[s.charAt(i)-'a']=i;
+        }
+        Stack<Character> st= new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if (bool[ch - 'a']) continue;
+            while(!st.isEmpty() && ch<st.peek() && position[st.peek()-'a']>i){
+                bool[st.pop()-'a']=false;
+            }
+            st.push(ch);
+            bool[ch-'a']=true;
+        }
+        StringBuilder str= new StringBuilder();
+        while(!st.isEmpty()){
+            str.append(st.pop());
+        }
+        return str.reverse().toString(); 
+    }
+}
